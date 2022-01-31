3d60e2324c17488ebe44ece63eddf504aaf3e465
VolTorian <voltorian.social@gmail.com>
2022-01-31 12:46:46 -0800

    i did work

---
+++ 99_bottles.js
@@ -0,0 +1,10 @@
+function bottleSong() {
+  // Write your code here!
+  var count = 99;
+  while (count > 1)
+  {
+    //console.log(count + " bottles of beer on the wall, " + count + "bottles of beer.\n Take one down and pass it around, " + count +)
+  }
+};
+
+bottleSong();
